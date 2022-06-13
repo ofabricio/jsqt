3c9cd7951e3b0d66d4f6ef7f14046236dcfcf372
Fabricio <fabriciogiac@gmail.com>
2022-06-12 22:07:09 -0300

    Remove dead code

--- jsqt.go
+++ jsqt.go
@@ -3,7 +3,6 @@ package jsqt
 import (
 	"strconv"
 	"strings"
-	"unicode"
 
 	. "github.com/ofabricio/scanner"
 )
@@ -12,18 +11,6 @@ func Get(jsn, qry string) Json {
 	src := New(jsn)
 	q := query{Scanner: Scanner(qry), Root: src}
 	return New(q.Parse(src))
-}
-
-func Get2(jsn, qry string) Json {
-	src := New(jsn)
-	q := query{Scanner: Scanner(qry), Root: src}
-	return New(q.ParseRoot(src))
-}
-
-func GetClosure(jsn, qry string) Json {
-	src := New(jsn)
-	q := query{Scanner: Scanner(qry), Root: src}
-	return New(q.ParseClosure(src))
 }
 
 func New(jsn string) Json {
@@ -37,9 +24,7 @@ type query struct {
 	Root Json
 }
 
-// #region Closure
-
-func (q *query) ParseClosure(j Json) string {
+func (q *query) Parse(j Json) string {
 	if v := q.ParseFunc(j); v != "" {
 		return v
 	}
@@ -67,7 +52,7 @@ func (q *query) ParseFunc(j Json) string {
 			return j.String()
 		}
 		if fname := q.TokenAnything(); fname != "" {
-			if v := q.CallFuncClosure(fname, j); v != "" {
+			if v := q.CallFunc(fname, j); v != "" {
 				if q.MatchByte(')') {
 					return v
 				}
@@ -84,7 +69,7 @@ func (q *query) TokenAnything() string {
 	})
 }
 
-func (q *query) CallFuncClosure(fname string, j Json) string {
+func (q *query) CallFunc(fname string, j Json) string {
 	if fname == "raw" {
 		return q.ParseFuncArg(j)
 	}
@@ -94,15 +79,6 @@ func (q *query) CallFuncClosure(fname string, j Json) string {
 	if fname == "." {
 		return j.String()
 	}
-	// if fname == "pipe" {
-	// for {
-	// 	v := q.ParseFuncArg(j)
-	// 	if v == "" {
-	// 		return j.String()
-	// 	}
-	// 	j = New(v)
-	// }
-	// }
 	if fname == "get" {
 		for {
 			key := q.ParseFuncArg(j)
@@ -115,18 +91,6 @@ func (q *query) CallFuncClosure(fname string, j Json) string {
 			j = j.Collect(key)
 		}
 	}
-	// if fname == "path" {
-	// 	for {
-	// 		key := q.ParseFuncArg(j)
-	// 		if key == "" {
-	// 			return j.String()
-	// 		}
-	// 		if key[0] == '"' {
-	// 			key = key[1 : len(key)-1]
-	// 		}
-	// 		j = j.Collect(key)
-	// 	}
-	// }
 	if fname == "obj" {
 		var out strings.Builder
 		out.WriteString("{")
@@ -199,7 +163,7 @@ func (q *query) Collect(j Json, f func(sub *query, item Json) string) string {
 	var arr strings.Builder
 	arr.WriteString("[")
 	ini := *q
-	j.IterateArray(func(i string, item Json) bool {
+	j.ForEach(func(i string, item Json) bool {
 		end := ini
 		if v := f(&end, item); v != "" {
 			if arr.Len() > 1 {
@@ -216,265 +180,12 @@ func (q *query) Collect(j Json, f func(sub *query, item Json) string) string {
 
 func (q *query) ForEach(j Json, f func(sub *query, item Json)) {
 	ini := *q
-	j.IterateArray(func(i string, item Json) bool {
+	j.ForEach(func(i string, item Json) bool {
 		end := ini
 		f(&end, item)
 		*q = end
 		return false
 	})
-}
-
-// #endregion Closure
-
-func (q *query) ParseRoot(j Json) string {
-	return q.ParseValue(j)
-}
-
-func (q *query) ParseValue(j Json) string {
-	if v := q.ParsePath(j); v != "" {
-		return v
-	}
-	if v := q.ParseRaw(); v != "" {
-		return v
-	}
-	return ""
-}
-
-func (q *query) ParsePath(j Json) string {
-	if !q.MatchByte('.') {
-		return j.String()
-	}
-	if v := q.ParseKey(); v != "" {
-		return q.ParsePath(j.Get(v))
-	}
-	return j.String()
-}
-
-func (q *query) ParseKey() string {
-	m := q.Mark()
-	if q.UtilMatchString('"') {
-		key := q.Token(m)
-		return key[1 : len(key)-1]
-	}
-	if q.MatchWhileRuneBy(q.IsName) {
-		return q.Token(m)
-	}
-	return ""
-}
-
-func (q *query) IsName(r rune) bool {
-	return unicode.IsLetter(r) || r == '_' || unicode.IsDigit(r)
-}
-
-func (q *query) ParseRaw() string {
-	return q.TokenRuneBy(unicode.IsPrint)
-}
-
-func (q *query) Parse(j Json) string {
-	if q.MatchByte('.') {
-		if q.EqualByte('|') {
-			return q.MatchFilter(j)
-		}
-	}
-	if key := q.MatchObjectKey(); key != "" {
-		return q.Parse(j.Get(key))
-	}
-	if obj := q.MatchObject(j); obj != "" {
-		return obj
-	}
-	if arr := q.MatchArray(j); arr != "" {
-		return arr
-	}
-	if name, args := q.MatchFunc(); name != "" {
-		return q.CallFunc(name, args, j)
-	}
-	if q.MatchByte('@') && q.Match("root") {
-		return q.Parse(q.Root)
-	}
-	if v := q.MatchRawValue(); v != "" {
-		return v
-	}
-	return j.String()
-}
-
-func (q *query) MatchFilter(j Json) string {
-	if q.MatchByte('|') {
-		v := q.Parse(j)
-		q.MatchByte(' ')
-		lh := q.Parse(j)
-		q.MatchByte('>')
-		rh := q.Parse(j)
-		q.MatchByte('|')
-		a, _ := strconv.Atoi(lh)
-		b, _ := strconv.Atoi(rh)
-		if a > b {
-			return q.Parse(New(v))
-		}
-	}
-	return ""
-}
-
-func (q *query) MatchRawValue() string {
-	if !q.MatchByte('!') {
-		return ""
-	}
-	// TODO: string.
-	return q.TokenByteBy(q.IsValue)
-}
-
-func (q *query) MatchValue() string {
-	if v := q.MatchRawValue(); v != "" {
-		return v
-	}
-	return q.MatchObjectKey()
-}
-
-func (q *query) MatchObjectKey() string {
-	if m := q.Mark(); q.UtilMatchString('"') {
-		str := q.Token(m)
-		return str[1 : len(str)-1] // Remove "".
-	}
-	return q.TokenByteBy(q.IsObjectKey)
-}
-
-func (q *query) MatchFunc() (string, string) {
-	if q.MatchByte('(') {
-		f := func() bool {
-			return q.MatchUntilAnyByte(' ', ')')
-		}
-		name, _ := q.TokenFor(f), q.MatchByte(' ')
-		args, _ := q.TokenFor(f), q.MatchByte(')')
-		return name, args
-	}
-	return "", ""
-}
-
-func (q *query) CallFunc(name, args string, j Json) string {
-	// TODO: add global function map.
-	if name == "flatten" {
-		return flatten(q, j, args)
-	}
-	if name == "size" {
-		return size(q, j, args)
-	}
-	if name == "omitempty" {
-		return omitempty(q, j, args)
-	}
-	if name == "default" {
-		return defaultValue(q, j, args)
-	}
-	if name == "collect" {
-		return collect(q, j, args)
-	}
-	if name == "merge" {
-		return merge(q, j, args)
-	}
-	return j.String()
-}
-
-func (q *query) MatchObject(j Json) string {
-	if q.MatchByte('{') {
-		var obj strings.Builder
-		obj.WriteString("{")
-		for q.MatchByte(',') || !q.MatchByte('}') {
-			key := q.ParseObjectKey()
-			if key == "" {
-				key = q.GetLastPathSegment()
-			}
-			if v := q.Parse(j); v != "" {
-				if obj.Len() > 1 {
-					obj.WriteString(",")
-				}
-				obj.WriteString(`"`)
-				obj.WriteString(key)
-				obj.WriteString(`":`)
-				obj.WriteString(v)
-			}
-		}
-		obj.WriteString("}")
-		return obj.String()
-	}
-	return ""
-}
-
-func (q *query) ParseObjectKey() string {
-	m := q.Mark()
-	if key := q.MatchObjectKey(); q.MatchByte(':') {
-		return key
-	}
-	q.Back(m)
-	return ""
-}
-
-func (q *query) GetLastPathSegment() string {
-	m := q.Mark()
-	key := q.MatchObjectKey()
-	for q.MatchByte('.') {
-		if k := q.MatchObjectKey(); k != "" {
-			key = k
-		}
-	}
-	q.Back(m)
-	return key
-}
-
-func (q *query) MatchArray(j Json) string {
-	if q.MatchByte('[') {
-		var obj strings.Builder
-		obj.WriteString("[")
-		for q.MatchByte(',') || !q.MatchByte(']') {
-			if v := q.Parse(j); v != "" {
-				if obj.Len() > 1 {
-					obj.WriteString(",")
-				}
-				obj.WriteString(v)
-			}
-		}
-		obj.WriteString("]")
-		return obj.String()
-	}
-	return ""
-}
-
-func (q *query) ParseJsonArrayItems(j Json, f func(string) string) {
-	end := *q
-	j.IterateArray(func(i string, v Json) bool {
-		sub := *q
-		if s := f(sub.Parse(v)); s != "" {
-			end = sub
-		}
-		return false
-	})
-	*q = end
-}
-
-func (q *query) FilterJsonArray(j Json, f func(string) string) string {
-	var arr strings.Builder
-	arr.WriteString("[")
-	q.ParseJsonArrayItems(j, func(v string) string {
-		if v = f(v); v != "" {
-			if arr.Len() > 1 {
-				arr.WriteString(",")
-			}
-			arr.WriteString(v)
-		}
-		return v
-	})
-	arr.WriteString("]")
-	return arr.String()
-}
-
-func identity(v string) string {
-	return v
-}
-
-func (q *query) IsObjectKey(r byte) bool {
-	return r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9' || r == '_'
-}
-
-func (q *query) IsValue(r byte) bool {
-	return r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9' || r == '_' ||
-		r == '-' || r == '+' || r == '.'
 }
 
 // #endregion Query
@@ -503,8 +214,8 @@ func (j *Json) iterate(path string, f func(string, Json) bool) {
 		v.iterate(path+k, f)
 		return false
 	}
-	j.IterateObject(fn)
-	j.IterateArray(fn)
+	j.ForEachKeyVal(fn)
+	j.ForEach(fn)
 }
 
 func (j *Json) Get(keyOrIndex string) (r Json) {
@@ -515,8 +226,8 @@ func (j *Json) Get(keyOrIndex string) (r Json) {
 		}
 		return false
 	}
-	j.IterateObject(f)
-	j.IterateArray(f)
+	j.ForEachKeyVal(f)
+	j.ForEach(f)
 	return r
 }
 
@@ -524,7 +235,7 @@ func (j *Json) Collect(keyOrIndex string) (r Json) {
 	if j.IsArray() && !(keyOrIndex[0] >= '0' && keyOrIndex[0] <= '9') {
 		var out strings.Builder
 		out.WriteString("[")
-		j.IterateArray(func(k string, v Json) bool {
+		j.ForEach(func(i string, v Json) bool {
 			if out.Len() > 1 {
 				out.WriteString(",")
 			}
@@ -533,9 +244,8 @@ func (j *Json) Collect(keyOrIndex string) (r Json) {
 		})
 		out.WriteString("]")
 		return New(out.String())
-	} else {
-		return j.Get(keyOrIndex)
 	}
+	return j.Get(keyOrIndex)
 }
 
 func (j *Json) IsObject() bool {
@@ -546,7 +256,7 @@ func (j *Json) IsArray() bool {
 	return j.EqualByte('[')
 }
 
-func (j *Json) IterateObject(f func(string, Json) bool) {
+func (j *Json) ForEachKeyVal(f func(string, Json) bool) {
 	if j.MatchByte('{') {
 		for !j.MatchByte('}') {
 			k, _ := j.TokenFor(j.MatchString), j.MatchByte(':')
@@ -558,7 +268,7 @@ func (j *Json) IterateObject(f func(string, Json) bool) {
 	}
 }
 
-func (j *Json) IterateArray(f func(string, Json) bool) {
+func (j *Json) ForEach(f func(string, Json) bool) {
 	if j.MatchByte('[') {
 		for i := 0; !j.MatchByte(']'); i++ {
 			k := strconv.Itoa(i)
@@ -588,11 +298,11 @@ func (j *Json) MatchRest() bool {
 }
 
 func (j *Json) MatchObject() bool {
-	return j.UtilMatchOpenCloseCount('{', '}')
+	return j.UtilMatchOpenCloseCount('{', '}', '"')
 }
 
 func (j *Json) MatchArray() bool {
-	return j.UtilMatchOpenCloseCount('[', ']')
+	return j.UtilMatchOpenCloseCount('[', ']', '"')
 }
 
 func (j *Json) MatchString() bool {
@@ -603,70 +313,29 @@ func (j *Json) MatchString() bool {
 
 // #region Functions
 
-func defaultValue(q *query, j Json, arg string) string {
-	v := q.Parse(j)
-	if v == "" {
-		return arg
-	}
-	return v
-}
-
-func collect(q *query, j Json, arg string) string {
-	return q.FilterJsonArray(j, identity)
-}
-
-func flatten(q *query, j Json, arg string) string {
-	v := q.FilterJsonArray(j, identity)
-	v = strings.TrimPrefix(v, "[")
-	v = strings.TrimSuffix(v, "]")
-	return v
-}
-
-func size(q *query, j Json, arg string) string {
-	c := 0
-	q.ParseJsonArrayItems(j, func(v string) string {
-		if v != "" {
-			c++
-		}
-		return v
-	})
-	return strconv.Itoa(c)
-}
-
-func omitempty(q *query, j Json, arg string) string {
-	v := q.Parse(j)
-	if v == "{}" {
-		return ""
-	}
-	if v == "[]" {
-		return ""
-	}
-	return v
-}
-
-func merge(q *query, j Json, arg string) string {
-	done := make(map[string]bool)
-	var b strings.Builder
-	b.WriteString("{")
-	q.ParseJsonArrayItems(j, func(v string) string {
-		j = New(v)
-		j.IterateObject(func(k string, v Json) bool {
-			if !done[k] {
-				if b.Len() > 1 {
-					b.WriteString(",")
-				}
-				b.WriteString(`"`)
-				b.WriteString(k)
-				b.WriteString(`":`)
-				b.WriteString(v.String())
-			}
-			done[k] = true
-			return false
-		})
-		return v
-	})
-	b.WriteString("}")
-	return b.String()
-}
+// func merge(q *query, j Json, arg string) string {
+// 	done := make(map[string]bool)
+// 	var b strings.Builder
+// 	b.WriteString("{")
+// 	q.ParseJsonArrayItems(j, func(v string) string {
+// 		j = New(v)
+// 		j.ForEachKeyVal(func(k string, v Json) bool {
+// 			if !done[k] {
+// 				if b.Len() > 1 {
+// 					b.WriteString(",")
+// 				}
+// 				b.WriteString(`"`)
+// 				b.WriteString(k)
+// 				b.WriteString(`":`)
+// 				b.WriteString(v.String())
+// 			}
+// 			done[k] = true
+// 			return false
+// 		})
+// 		return v
+// 	})
+// 	b.WriteString("}")
+// 	return b.String()
+// }
 
 // #endregion Functions
--- jsqt_test.go
+++ jsqt_test.go
@@ -7,23 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetClosure_Isolated(t *testing.T) {
-
-	tt := []struct {
-		give string
-		when string
-		then string
-	}{
-		{give: `{"a":[{"b":{}}]}`, when: `(collect (get a) () (get b) (get c))`, then: `[]`},
-		// {give: `{"a":[{"b":{"c":3}},{"b":{}}]}`, when: `(collect (get a) () (get b) (get c))`, then: `[3,4]`},
-	}
-	for _, tc := range tt {
-		r := GetClosure(tc.give, tc.when)
-		assert.Equal(t, tc.then, r.String(), "TC: %v", tc)
-	}
-}
-
-func TestGetClosure(t *testing.T) {
+func TestGet(t *testing.T) {
 
 	tt := []struct {
 		give string
@@ -78,168 +62,6 @@ func TestGetClosure(t *testing.T) {
 		{give: ``, when: `(raw -3)`, then: `-3`},
 		{give: ``, when: `(raw 1.2)`, then: `1.2`},
 	}
-	for _, tc := range tt {
-		r := GetClosure(tc.give, tc.when)
-		assert.Equal(t, tc.then, r.String(), "TC: %v", tc)
-	}
-}
-
-func TestGet2(t *testing.T) {
-
-	tt := []struct {
-		give string
-		when string
-		then string
-	}{
-		{give: `[{"a":3},{"a":4}]`, when: `.0.a`, then: `3`},
-		{give: `[2,3,4]`, when: `.3`, then: ``},
-		{give: `[2,3,4]`, when: `.2`, then: `4`},
-		{give: `[2,3,4]`, when: `.1`, then: `3`},
-		{give: `[2,3,4]`, when: `.0`, then: `2`},
-		{give: `{"aa":{"bb":{"cc":3}}}`, when: `.aa.bb.cc`, then: `3`},
-		{give: `{"a":{"b":3}}`, when: `.a.b`, then: `3`},
-		{give: `{"a b":3}`, when: `."a b"`, then: `3`},
-		{give: `{"a":3}`, when: `.a`, then: `3`},
-		{give: `3`, when: `.`, then: `3`},
-		{give: ``, when: `.`, then: ``},
-		{give: ``, when: `{}`, then: `{}`},
-		{give: ``, when: `[]`, then: `[]`},
-		{give: ``, when: `null`, then: `null`},
-		{give: ``, when: `true`, then: `true`},
-		{give: ``, when: `false`, then: `false`},
-		{give: ``, when: `3e2`, then: `3e2`},
-		{give: ``, when: `3`, then: `3`},
-		{give: ``, when: `-3`, then: `-3`},
-		{give: ``, when: `1.2`, then: `1.2`},
-	}
-	for _, tc := range tt {
-		r := Get2(tc.give, tc.when)
-		assert.Equal(t, tc.then, r.String(), "TC: %v", tc)
-	}
-}
-
-func TestGet(t *testing.T) {
-
-	tt := []struct {
-		give string
-		when string
-		then string
-	}{
-		// Filter.
-		{
-			give: `[{"a":[1,5],"b":3},{"a":[3,6],"b":7}]`,
-			when: `.(collect).|a b>!4|.(collect).|. .>!4|`,
-			then: `[[6]]`,
-		},
-		{
-			give: `[3,4,5,6]`,
-			when: `.(collect).|. .>@root.1|`,
-			then: `[5,6]`,
-		},
-		{
-			give: `[3,4,5,6]`,
-			when: `.(collect).|. .>!4|`,
-			then: `[5,6]`,
-		},
-		{
-			give: `{"a":[{"b":[{"c":[{"d":3},{"d":4}]},{"c":[{"d":5},{"d":6}]}]}]}`,
-			when: `a.(collect).b.(collect).c.(omitempty).(collect).|d d>!4|`,
-			then: `[[[5,6]]]`,
-		},
-		{
-			give: `{"a":[{"b":[{"c":[{"d":3},{"d":4}]},{"c":[{"d":5},{"d":6}]}]}]}`,
-			when: `a.(collect).b.(collect).c.(collect).|d d>!4|`,
-			then: `[[[],[5,6]]]`,
-		},
-		{
-			give: `{"a":[{"b":3,"c":4},{"b":5,"c":6}]}`,
-			when: `a.(collect).|b c>!4|`,
-			then: `[5]`,
-		},
-		{
-			give: `{"a":[{"b":3,"c":4},{"b":5,"c":6}]}`,
-			when: `a.(collect).|{b} c>!4|`,
-			then: `[{"b":5}]`,
-		},
-		// Merge function.
-		{give: `[{"a":3},{"b":4},{"c":5}]`, when: `.(merge)`, then: `{"a":3,"b":4,"c":5}`},
-		// Default function.
-		{give: `[{"b":3},{"c":4},{"b":5}]`, when: `.(collect).(default 0).b`, then: `[3,0,5]`},
-		// OmitEmpty Function.
-		{give: `{"a":[[3],[]]}`, when: `.a.(size).(omitempty)`, then: `1`},
-		{give: `{"a":[[3],[]]}`, when: `.a.(collect).(omitempty)`, then: `[[3]]`},
-		{give: `{"a":[{"d":3},{"e":4}]}`, when: `.a.(size).(omitempty).{d}`, then: `1`},
-		{give: `{"a":[{"d":3},{"e":4}]}`, when: `.a.(collect).(omitempty).{d}`, then: `[{"d":3}]`},
-		// Size function.
-		{give: `{"a":[{"b":[{"c":[{"d":3}]}]}]}`, when: `.a.(flatten).b.(collect).c.(size).d`, then: `[1]`},
-		{give: `{"a":[{"d":3},{"e":4}]}`, when: `.a.(size).d`, then: `1`},
-		{give: `{"a":[3,4]}`, when: `.a.(size)`, then: `2`},
-		{give: `{"a":[3]}`, when: `.a.(size)`, then: `1`},
-		{give: `{"a":[]}`, when: `.a.(size)`, then: `0`},
-		// Flatten function.
-		{give: `{"a":{"b":{"c":[{"d":"one","e":{"f":[{"g":{"h":{"i":{"j":[{"k":{"l":"hi"}}]}}}}]}},{"d":"two","e":{"f":[{"g":{"h":{"i":{"j":[]}}}}]}}]}}}`, when: `.a.b.c.(collect).{d:.d,e:.e.f.(flatten).g.h.i.j.(collect).k.l}`, then: `[{"d":"one","e":["hi"]},{"d":"two","e":[]}]`},
-		{give: `{"a":[{"b":[{"c":[{"d":3}]}]}]}`, when: `.a.(flatten).b.(flatten).c.(flatten).d`, then: `3`},
-		{give: `{"a":[{"b":[{"c":[{"d":3}]}]}]}`, when: `.a.(flatten).b.(flatten).c.(collect).d`, then: `[3]`},
-		{give: `{"a":[{"b":[{"c":[{"d":3}]}]}]}`, when: `.a.(flatten).b.(collect).c.(collect).d`, then: `[[3]]`},
-		{give: `{"a":[{"b":[{"c":3}]}]}`, when: `.a.(flatten).b.(collect).c`, then: `[3]`},
-		// First context (dot) is optional.
-		{give: `{"a":3,"b":4}`, when: `{x:a}`, then: `{"x":3}`},
-		{give: `{"a":3,"b":4}`, when: `.{x:a}`, then: `{"x":3}`},
-		{give: `{"a":3,"b":4}`, when: `.{x:.a}`, then: `{"x":3}`},
-		{give: `{"a":3,"b":4}`, when: `{x:.a}`, then: `{"x":3}`},
-		{give: `{"a":3,"b":4}`, when: `.{.a}`, then: `{"a":3}`},
-		{give: `{"a":3,"b":4}`, when: `{.a}`, then: `{"a":3}`},
-		{give: `{"a":3,"b":4}`, when: `.{a}`, then: `{"a":3}`},
-		// Root node access.
-		{give: `{"a":{"b":{"c":{"d":3}}},"e":4}`, when: `{a.b.{c.d,x:@root.e}}`, then: `{"b":{"d":3,"x":4}}`},
-		// Array generation.
-		{give: `3`, when: `[.,.]`, then: `[3,3]`},
-		{give: `3`, when: `[.,@root]`, then: `[3,3]`},
-		{give: `3`, when: `[{x:.}]`, then: `[{"x":3}]`},
-		{give: `{"a":[3,4]}`, when: `a.(collect).[.]`, then: `[[3],[4]]`},
-		{give: `{"a":{"b":[3,4]}}`, when: `[a.b.1,a.b.0]`, then: `[4,3]`},
-		// General.
-		{give: `{"a":{"a b c":3}}`, when: `{a."a b c","x y z":a."a b c"}`, then: `{"a b c":3,"x y z":3}`},
-		{give: `{"a":{"b":{"c":3}}}`, when: `a.{b.{c}}`, then: `{"b":{"c":3}}`},
-		{give: `{"a":{"b":{"c":[{"d":"one","e":{"f":[{"g":{"h":{"i":{"j":[{"k":{"l":"hi"}}]}}}}]}},{"d":"two","e":{"f":[{"g":{"h":{"i":{"j":[]}}}}]}}]}}}`, when: `a.b.c.(collect).{d:d,e:e.f.(collect).g.h.i.j.(collect).k.l}`, then: `[{"d":"one","e":[["hi"]]},{"d":"two","e":[[]]}]`},
-		{give: `[{"b":3},{"c":4},{"b":5}]`, when: `.(collect)`, then: `[{"b":3},{"c":4},{"b":5}]`},
-		{give: `[{"b":3},{"c":4},{"b":5}]`, when: `.(collect).b`, then: `[3,5]`},
-		{give: `{"a":{"b":{"c":[{"d":1,"e":{"f":[{"g":{"h":{"i":{"j":[{"k":3}]}}}}]}}]}}}`, when: `a.b.c.(collect).{d,data:e.f.(collect).g.h.i.j.(collect).k}`, then: `[{"d":1,"data":[[3]]}]`},
-		{give: `{"a":{"b":[3,4]}}`, when: `{.a.b.(collect)}`, then: `{"b":[3,4]}`},
-		{give: `{"a":[{"b":3}]}`, when: `a`, then: `[{"b":3}]`},
-		{give: `{"a":[{"b":3}]}`, when: `a.(collect)`, then: `[{"b":3}]`},
-		{give: `{"a":[{"b":3}]}`, when: `a.(collect).b`, then: `[3]`},
-		{give: `{"a":[{"b":[{"c":3}]}]}`, when: `a.(collect).b`, then: `[[{"c":3}]]`},
-		{give: `{"a":[{"b":[{"c":3}]}]}`, when: `a.(collect).b.(collect)`, then: `[[{"c":3}]]`},
-		{give: `{"a":[{"b":[{"c":3}]}]}`, when: `a.(collect).b.(collect).c`, then: `[[3]]`},
-		{give: `{"a":[{"b":[{"c":[{"d":3}]}]}]}`, when: `a.(collect).b.(collect).c`, then: `[[[{"d":3}]]]`},
-		{give: `{"a":[{"b":[{"c":[{"d":3}]}]}]}`, when: `a.(collect).b.(collect).c.(collect)`, then: `[[[{"d":3}]]]`},
-		{give: `{"a":[{"b":[{"c":[{"d":3}]}]}]}`, when: `a.(collect).b.(collect).c.(collect).d`, then: `[[[3]]]`},
-		{give: `{"a":3}`, when: `{x:a}`, then: `{"x":3}`},
-		{give: `{"a":{"b":3}}`, when: `{x:a.b}`, then: `{"x":3}`},
-		{give: `{"a":[{"b":1},{"c":2},{"b":3}]}`, when: `a.(collect).{x:b}`, then: `[{"x":1},{},{"x":3}]`},
-		{give: `{"a":[{"b":{"c":2}},{"b":{"d":3}}]}`, when: `a.(collect).{b.c,b.d}`, then: `[{"c":2},{"d":3}]`},
-		{give: `{"a":[{"b":{"c":2}},{"b":{"d":3}}]}`, when: `a.(collect).{b.c}`, then: `[{"c":2},{}]`},
-		{give: `{"a":[{"b":1},{"c":2},{"b":3}]}`, when: `a.(collect).{b,.c}`, then: `[{"b":1},{"c":2},{"b":3}]`},
-		{give: `{"a":[{"b":1},{"c":2},{"b":3}]}`, when: `a.(collect).{b}`, then: `[{"b":1},{},{"b":3}]`},
-		{give: `{"a":[{"b":1},{"c":2},{"b":3}]}`, when: `a.(collect).b`, then: `[1,3]`},
-		{give: `{"a":[2,3]}`, when: `a.1`, then: `3`},
-		{give: `{"a":2}`, when: `b`, then: ``},
-		{give: `{"a":{"b":2}}`, when: `a.b`, then: `2`},
-		{give: `{"a":[]}`, when: `a`, then: `[]`},
-		{give: `{"a":{}}`, when: `a`, then: `{}`},
-		{give: `{"a":2}`, when: `a`, then: `2`},
-		{give: `{"a":"a\"b"}`, when: `a`, then: `"a\"b"`},
-		{give: `2`, when: `.`, then: `2`},
-		{give: `-2`, when: `.`, then: `-2`},
-		{give: `false`, when: `.`, then: `false`},
-		{give: `null`, when: `.`, then: `null`},
-		{give: `{}`, when: `.`, then: `{}`},
-		{give: `[]`, when: `.`, then: `[]`},
-		{give: `[1,2]`, when: `.`, then: `[1,2]`},
-		{give: `"a"`, when: `.`, then: `"a"`},
-	}
-
 	for _, tc := range tt {
 		r := Get(tc.give, tc.when)
 		assert.Equal(t, tc.then, r.String(), "TC: %v", tc)
@@ -325,7 +147,7 @@ func ExampleJson_Iterate() {
 	// .d 1
 }
 
-func TestJsonIterateObject(t *testing.T) {
+func TestJsonForEachKeyVal(t *testing.T) {
 	tt := []struct {
 		inp string
 		out []string
@@ -339,7 +161,7 @@ func TestJsonIterateObject(t *testing.T) {
 	for _, tc := range tt {
 		var r []string
 		j := New(tc.inp)
-		j.IterateObject(func(k string, v Json) bool {
+		j.ForEachKeyVal(func(k string, v Json) bool {
 			r = append(r, k, v.String())
 			return false
 		})
@@ -347,7 +169,7 @@ func TestJsonIterateObject(t *testing.T) {
 	}
 }
 
-func TestJsonIterateArray(t *testing.T) {
+func TestJsonForEach(t *testing.T) {
 	tt := []struct {
 		inp string
 		out []string
@@ -361,63 +183,10 @@ func TestJsonIterateArray(t *testing.T) {
 	for _, tc := range tt {
 		var r []string
 		j := New(tc.inp)
-		j.IterateArray(func(k string, v Json) bool {
+		j.ForEach(func(k string, v Json) bool {
 			r = append(r, k, v.String())
 			return false
 		})
 		assert.Equal(t, tc.out, r, tc.inp)
-	}
-}
-
-func TestJson_MatchObject(t *testing.T) {
-	tt := []struct {
-		inp string
-		exp bool
-	}{
-		{inp: "", exp: false},
-		{inp: "{", exp: false},
-		{inp: "}", exp: false},
-		{inp: "[{}]", exp: false},
-		{inp: "{}", exp: true},
-		{inp: `{"a":"b"}`, exp: true},
-		{inp: `{"a":"b","c":"d"}`, exp: true},
-		{inp: `{"a":{},"c":[]}`, exp: true},
-		{inp: `{"a":{"b":1},"c":[2]}`, exp: true},
-	}
-	for _, tc := range tt {
-		j := New(tc.inp)
-		m := j.Mark()
-		ok := j.MatchObject()
-		assert.Equal(t, tc.exp, ok, tc.inp)
-		if ok {
-			assert.Equal(t, tc.inp, j.Token(m), tc.inp)
-		}
-	}
-}
-
-func TestJson_MatchArray(t *testing.T) {
-	tt := []struct {
-		inp string
-		exp bool
-	}{
-		{inp: "", exp: false},
-		{inp: "{[]}", exp: false},
-		{inp: "[]", exp: true},
-		{inp: "[true]", exp: true},
-		{inp: "[false]", exp: true},
-		{inp: "[null]", exp: true},
-		{inp: "[123]", exp: true},
-		{inp: "[{}]", exp: true},
-		{inp: "[[]]", exp: true},
-		{inp: `[true,1,false,null,[],{},"a"]`, exp: true},
-	}
-	for _, tc := range tt {
-		j := New(tc.inp)
-		m := j.Mark()
-		ok := j.MatchArray()
-		assert.Equal(t, tc.exp, ok, tc.inp)
-		if ok {
-			assert.Equal(t, tc.inp, j.Token(m), tc.inp)
-		}
 	}
 }
